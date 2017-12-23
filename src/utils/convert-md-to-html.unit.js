eb152ddfe28f9d32d2612c8adb662575431f2262
Rachel Nabors <rachelnabors@users.noreply.github.com>
2017-12-22 19:17:22 -0800

    Fixing test results for converting md.

--- src/utils/convert-md-to-html.unit.js
+++ src/utils/convert-md-to-html.unit.js
@@ -4,6 +4,6 @@ describe('@utils/convert-md-to-html', () => {
   it('transforms # into <h1>', () => {
     const md = "# I'm an H1."
     const html = convertMdToHtml(md)
-    expect(html).toEqual(`<h1>I'm an h1</h1>`)
+    expect(html).toEqual(`<h1>I'm an H1.</h1>\n`)
   })
 })
